26ae39e812db7d6249e0a2d8c688fdf9e18d78aa
YuriOlivs <yuri.silva@sptech.school>
2024-03-29 17:39:03 -0300

    update: adicionando arquivo de cores

---
+++ values/colors.js
@@ -0,0 +1,17 @@
+export const Colors = {
+   //tons de laranja
+   orange100: '#FFEAE0',
+   orange300: '#FFA877',
+   orange500: '#FF7425',
+   orange600: '#CE5F20',
+   //tons de cinza/preto
+   gray900: '#131313',
+   gray800: '#191919',
+   gray700: '#242424',
+   gray600: '#323232',
+   gray200: '#D7D7D7',
+   gray100: '#FFF9F6',
+   //tons variados
+   green: '#3FB03D',
+   red: '#FF3838'
+}
