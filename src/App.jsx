c22f3bab71cbe65a994eea3b91ac71ed5f5edaa8
YuriOlivs <yuri.silva@sptech.school>
2024-03-30 11:40:26 -0300

    feat: iniciando landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-
+import LandingPage from './pages/LandingPage/LandingPage';
 import './App.css'
 
 function App() {
   return (
     <>
-
+      <LandingPage />
     </>
   )
 }
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import './LadingPage.styles.css'
+
+export default function LandingPage() {
+   return (
+      <>
+         <header>
+            <div className="bg-image"></div>
+         </header>
+      </>
+   )
+}
