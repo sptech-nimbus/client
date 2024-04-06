50fc97371a8dca07d78a60839e630dc249d24e15
zzzmiike <michael.silva@sptech.school>
2024-04-06 14:46:12 -0300

    feat: implementando a conexão do time

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,10 @@
 //configurações da api
 const config = {
+<<<<<<< Updated upstream
    baseURL: ''
+=======
+   baseURL: 'http://localhost:3000'
+>>>>>>> Stashed changes
 }
 
 export default config;
