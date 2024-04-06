936e436dc04c933018f16140b5b64821f05b755a
zzzmiike <michael.silva@sptech.school>
2024-04-06 14:46:55 -0300

    Revert "feat: implementando a conexão do time"
    This reverts commit 50fc97371a8dca07d78a60839e630dc249d24e15.

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,6 @@
 //configurações da api
 const config = {
-<<<<<<< Updated upstream
    baseURL: ''
-=======
-   baseURL: 'http://localhost:3000'
->>>>>>> Stashed changes
 }
 
 export default config;
--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,18 @@
 import axios from "axios";
 import config from "./config";
 
-const path = "users";
-
 //ajustar o endpoint para o endpoint correto da api
-async function getUserById(headers = {}) {
+async function getUser(headers) {
    try {
-<<<<<<< Updated upstream
       const response = await axios.get(`${config.baseURL}/user`, headers);
-=======
-      const response = await axios.get(`${config.baseURL}/${path}`, headers);
->>>>>>> Stashed changes
       return response;
    }
    catch(err) {
+      console.error("Erro na requisição da API.", err);
       throw err
    }
 }
 
-<<<<<<< Updated upstream
 async function postUser(body, headers) {
    try {
       const response = await axios.post(`${config.baseURL}/user`, {headers: headers, body: body});
@@ -26,19 +20,10 @@ async function postUser(body, headers) {
    }
    catch(err) {
       console.error("Erro na requisição da API.", err);
-=======
-async function postUser(body = {}, headers = {}) {
-   try {
-      const response = await axios.post(`${config.baseURL}/${path}`, body);
-      return response;
-   }
-   catch(err) {
->>>>>>> Stashed changes
       throw err;
    }
 }
 
-<<<<<<< Updated upstream
 async function putUser(body, headers) {
    try {
       const response = await axios.put(`${config.baseURL}/user`, {headers: headers, body: body});
@@ -46,19 +31,10 @@ async function putUser(body, headers) {
    }
    catch(err) {
       console.error("Erro na requisição da API.", err);
-=======
-async function putUser(body = {}, headers = {}) {
-   try {
-      const response = await axios.put(`${config.baseURL}/${path}`, body);
-      return response;
-   }
-   catch(err) {
->>>>>>> Stashed changes
       throw err;
    }
 }
 
-<<<<<<< Updated upstream
 async function deleteUser(params) {
    try {
       const response = await axios.delete(`${config.baseURL}/user/${id}`, { params });
@@ -66,52 +42,15 @@ async function deleteUser(params) {
    }
    catch(err) {
       console.error("Erro na requisição da API.", err);
-=======
-async function deleteUser(params = {}) {
-   try {
-      const response = await axios.delete(`${config.baseURL}/${path}`, body);
-      return response;
-   }
-   catch(err) {
-      throw err;
-   }
-}
-
-async function changePassword(id, body = {}) {
-   try {
-      const response = await axios.patch(`${config.baseURL}/${path}/change-password/${id}`, body);
-      return response
-   }
-   catch(err) {
-      throw err;
-   }
-}
-
-async function login(body, headers = {}) {
-   try {
-      const response = await axios.post(`${config.baseURL}/${path}/login`);
-      return response;
-   }
-   catch(err) {
->>>>>>> Stashed changes
       throw err;
    }
 }
 
 const user = {
-<<<<<<< Updated upstream
    get: getUser,
    post: postUser,
    put: putUser,
    delete: deleteUser,
-=======
-   get: getUserById,
-   post: postUser,
-   put: putUser,
-   delete: deleteUser,
-   changePassword: changePassword,
-   login: login,
->>>>>>> Stashed changes
 }
 
 export default user;
