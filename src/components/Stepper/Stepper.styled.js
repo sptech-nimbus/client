70ea3d3dde227ded3fbf1836cdbce5de91eea487
zzzmiike <michael.silva@sptech.school>
2024-04-15 20:31:00 -0300

    fix: ajustando valor dos Stepper

--- src/components/Stepper/Stepper.styled.js
+++ src/components/Stepper/Stepper.styled.js
@@ -4,9 +4,9 @@ import Colors from "@utils/Colors";
 export const Step = styled.div`
    width: 1rem;
    height: 1rem;
-   background-color: ${props => props.state == 'pending' ? Colors.orange100 : Colors.orange500 };
+   background-color: ${props => props.state == 'pending' ? Colors.orange100 : Colors.orange500};
    box-sizing: border-box;
-   border: .rem solid ${props => props.state == 'complete' ? Colors.orange500 : Colors.orange100};
+   border: 0.2rem solid ${props => props.state == 'complete' ? Colors.orange500 : Colors.orange100};
    border-radius: 50%;
 
    @media (min-width: 1700px) {
@@ -19,19 +19,19 @@ export const Stepper = styled.div`
    display: flex;
    justify-content: center;
    align-items: center;
-   gap: .rem;
+   gap: 0.5rem;
 `
 
 export const Line = styled.div`
    display: flex;
    width: fit-content;
    height: fit-content;
-   gap: .rem;
+   gap: 0.3rem;
 `
 
 export const Circle = styled.div`
-   width: .rem;
-   height: .rem;
+   width: 0.4rem;
+   height: 0.4rem;
    border-radius: 50%;
-   background-color: ${props => props.state == 'pending' ? Colors.orange100 : Colors.orange500 };
+   background-color: ${props => props.state == 'pending' ? Colors.orange100 : Colors.orange500};
 `
