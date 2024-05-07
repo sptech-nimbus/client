b7e0460eea482cdbfb4bbf44ed47154adae9f2f8
YuriOlivs <yuri.silva@sptech.school>
2024-05-07 10:00:41 -0300

    feat: atualizando handles do login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,34 +1,81 @@
-import * as S from "./Login.styles";
 import { useState } from "react";
+import * as S from "./Login.styles";
+
+import { Envelope } from "@phosphor-icons/react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 import Background from "@components/Background/Background";
 import Input from "@components/Input/Input";
 import Label from "@components/Label/Label";
-import { Envelope } from "@phosphor-icons/react";
 import Button from "@components/Button/Button";
-import { useNavigate } from "react-router-dom";
+
+import user from "@api/user";
 
 export default function Login() {
    const navigate = useNavigate();
-   const [email, setEmail] = useState('');
-   const [password, setPassword] = useState('');
+   const [userData, setUserData] = useState({
+      email: '',
+      password: ''
+   });
 
    const handleEmailChange = (e) => {
       const { value } = e.target;
-      setEmail(value);
+      setUserData({
+         ...userData,
+         email: value
+      });
    }
 
    const handlePasswordChange = (e) => {
       const { value } = e.target;
-      setPassword(value);
+      setUserData({
+         ...userData,
+         password: value
+      });
    }
+
+   const handleFormSubmit = (e) =>{
+      e.preventDefault();
+
+      if(userData.email && userData.password) {
+         user.login(userData)
+         .then(response => {
+            sessionStorage.setItem('token', response.data.data.token);
+            navigate('/home');
+         })
+         .catch(err => {
+            toast.error('Credenciais inválidas');
+            console.log(err);
+         })
+      }
+      else {
+         toast.error('Preencha todos os campos')
+      }
+   }
+
 
    return (
       <S.Header>
+         <ToastContainer
+                position={toastPosition}
+                autoClose={8000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                theme="dark"
+                limit={3}
+         /> 
+
          <Background.Login />
          <S.Title>
             Login
          </S.Title>
-         <S.Form>
+         <S.Form onSubmit={handleFormSubmit}>
             <Input.Google />
             <S.LineContainer>
                <S.Line />
@@ -38,22 +85,22 @@ export default function Login() {
             <S.InputsContainer>
                <Label >
                   Insira seu email
-                       <Input.Default
-                           placeholder={'seu@email.com'}
-                           value={email}
-                           onChange={handleEmailChange}
-                           >                    
+                  <Input.Default
+                     placeholder={'seu@email.com'}
+                     value={userData.email}
+                     onChange={handleEmailChange}
+                  >                    
                      <Envelope />
                   </Input.Default>
                </Label>
                <Label >
                   Insira sua senha
-                       <Input.Password
-                           placeholder={'**********'}
-                           value={password}
-                           hasIcon 
-                           onChange={handlePasswordChange}
-                           />
+                  <Input.Password
+                     placeholder={'**********'}
+                     value={userData.password}
+                     hasIcon 
+                     onChange={handlePasswordChange}
+                  />
                </Label>
             </S.InputsContainer>
             <Button.Primary 
@@ -61,7 +108,6 @@ export default function Login() {
                size={'md'}
                width={'100%'}
                fontSize={'1.5rem'}
-               onClick={() => navigate('/home')}
             />
             <S.FormFooter>
                <underlined>
