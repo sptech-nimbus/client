b0a2da0a29f91511f144a900ace5ebfa771cbb40
YuriOlivs <yuri.silva@sptech.school>
2024-05-07 11:38:09 -0300

    feat: atualizando formulários de cadastro

--- src/pages/Register/AthleteRegister/FormStepFour.jsx
+++ src/pages/Register/AthleteRegister/FormStepFour.jsx
@@ -16,20 +16,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 export default function FormStepFour({onSubmit}) {
-    const [teamName, setTeamName] = useState('');
     const [teamCode, setTeamCode] = useState('');
-    const [category, setCategory] = useState('');
-    const [teamPicture, setTeamPicture] = useState('');
-    const [local, setLocal] = useState('');
-    const [chkAmateur, setChkAmateur] = useState(false);
-
-    const [teamNameErr, setTeamNameErr] = useState(false);
-    const [teamCodeErr, setTeamCodeErr] = useState(false);
-    const [categoryErr, setCategoryErr] = useState(false);
-
-    const [teamNameTtpOpen, setTeamNameTtpOpen] = useState(false);
     const [teamCodeTtpOpen, setTeamCodeTtpOpen] = useState(false);
-    const [teamPictureTtpOpen, setTeamPictureTtpOpen] = useState(false);
 
     const [toastPosition, setToastPosition] = useState('top-right');
 
@@ -44,65 +32,24 @@ export default function FormStepFour({onSubmit}) {
         }
     }, [isBelow1050]);
 
-    function handleLocalChange(e) {
-        const { value } = e.target;
-        setLocal(value);
-    }
-
-    function handleTeamNameChange(e) {
-        const { value } = e.target;
-        setTeamName(value);
-    }
-
     function handleTeamCodeChange(e) {
         const { value } = e.target;
         setTeamCode(value);
-    }
-    function handleCategoryChange(e) {
-        const { value } = e.target;
-        setCategory(value);
-    }
-    function handleTeamPictureChange(e) {
-        setTeamPicture(e.target.files[0]);
-    }
-    function handleChkAmateur() {
-        setChkAmateur(!chkAmateur);
-    }
-
-    function handleTeamNameTtpChange() {
-        setTeamNameTtpOpen(!teamNameTtpOpen);
     }
 
     function handleTeamCodeTtpChange() {
         setTeamCodeTtpOpen(!teamCodeTtpOpen);
     }
 
-    function handleTeamPictureTtpChange() {
-        setTeamPictureTtpOpen(!teamPictureTtpOpen);
-    }
     function handleSubmit(e) {
         e.preventDefault();
 
-        console.log("entremo")
-
         if (TeamCodeValidation(teamCode)) {
-            console.log("Enviando solicita��o com o c�digo do time existente:", teamCode);
+            console.log("Enviando solicitação com o código do time existente: ", teamCode);
             onSubmit(teamCode);
         }
-        else if (
-            TextValidation(teamName) &&
-            CategoryValidation(category) &&
-            ImageValidation(teamPicture)) {
-            
-            onSubmit({ teamName, category, teamPicture, chkAmateur, local });
-        }
         else {
-            if (teamCode) {
-                if (!TeamCodeValidation(teamCode)) toast.error('C�digo inserido � inv�lido');
-            } else {
-                if (!TextValidation(teamName)) toast.error('Nome do time � inv�lido');
-                if (!ImageValidation(teamPicture)) toast.error('A extens�o de arquivo inserida � inv�lida');
-            }
+            if (!TeamCodeValidation(teamCode)) toast.error('Código inserido é inválido');
         }
     }
 
@@ -130,7 +77,6 @@ export default function FormStepFour({onSubmit}) {
                             onChange={handleTeamCodeChange}
                             onFocus={handleTeamCodeTtpChange}
                             onBlur={handleTeamCodeTtpChange}
-                            disabled={teamName || category || teamPicture ? true : false}
                             maxLength={6}
                         />
                         {
--- src/pages/Register/TrainerRegister/FormStepThree.jsx
+++ src/pages/Register/TrainerRegister/FormStepThree.jsx
@@ -16,16 +16,14 @@ import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 export default function FormStepThree({onSubmit}) {
-    const [teamName, setTeamName] = useState('');
-    const [teamCode, setTeamCode] = useState('');
-    const [category, setCategory] = useState('');
-    const [teamPicture, setTeamPicture] = useState('');
-    const [local, setLocal] = useState('');
-    const [chkAmateur, setChkAmateur] = useState(false);
-
-    const [teamNameErr, setTeamNameErr] = useState(false);
-    const [teamCodeErr, setTeamCodeErr] = useState(false);
-    const [categoryErr, setCategoryErr] = useState(false);
+    const [teamData, setTeamData] = useState({
+        name: '',
+        code: '',
+        category: '',
+        picture: '',
+        local: '',
+        chkAmateur: false
+    });
 
     const [teamNameTtpOpen, setTeamNameTtpOpen] = useState(false);
     const [teamCodeTtpOpen, setTeamCodeTtpOpen] = useState(false);
@@ -46,27 +44,45 @@ export default function FormStepThree({onSubmit}) {
 
     function handleLocalChange(e) {
         const { value } = e.target;
-        setLocal(value);
+        setTeamData({
+            ...teamData,
+            local: value
+        });
     }
 
     function handleTeamNameChange(e) {
         const { value } = e.target;
-        setTeamName(value);
+        setTeamData({
+            ...teamData,
+            name: value
+        });
     }
 
     function handleTeamCodeChange(e) {
         const { value } = e.target;
-        setTeamCode(value);
+        setTeamData({
+            ...teamData,
+            code: value
+        });
     }
     function handleCategoryChange(e) {
         const { value } = e.target;
-        setCategory(value);
+        setTeamData({
+            ...teamData,
+            category: value
+        });
     }
     function handleTeamPictureChange(e) {
-        setTeamPicture(e.target.files[0]);
+        setTeamData({
+            ...teamData,
+            picture: e.target.files[0]
+        });
     }
+
     function handleChkAmateur() {
-        setChkAmateur(!chkAmateur);
+        setTeamData({
+            chkAmateur: !teamData.chkAmateur
+    });
     }
 
     function handleTeamNameTtpChange() {
@@ -83,29 +99,23 @@ export default function FormStepThree({onSubmit}) {
     function handleSubmit(e) {
         e.preventDefault();
 
-        if (TeamCodeValidation(teamCode)) {
-            console.log("Enviando solicitação com o código do time existente:", teamCode);
-            onSubmit(teamCode);
+        if (TeamCodeValidation(teamData.code)) {
+            console.log("Enviando solicitação com o código do time existente: ", teamData.code);
+            onSubmit(teamData.code);
         }
         else if (
-            TextValidation(teamName) && 
-            TextValidation(category) && 
-            ImageValidation(teamLogo)) 
+            TextValidation(teamData.name) && 
+            TextValidation(teamData.category) && 
+            ImageValidation(teamData.picture)) 
         {
-            console.log("Enviando solicitação com os seguintes dados:");
-            console.log("Nome do time:", teamName);
-            console.log("Categoria:", category);
-            console.log("Logo do time:", teamLogo);
-            console.log("Amador:", chkAmateur);
-
-            onSubmit({ teamName, category, teamLogo, chkAmateur });
+            onSubmit(teamData);
         }
         else {
-            if (teamCode) {
-                if (!TeamCodeValidation(teamCode)) toast.error('Código inserido é inválido');
+            if (teamData.code) {
+                if (!TeamCodeValidation(teamData.code)) toast.error('Código inserido é inválido');
             } else {
-                if (!TextValidation(teamName)) toast.error('Nome do time é inválido');
-                if (!ImageValidation(teamPicture)) toast.error('A extensão de arquivo inserida é inválida');
+                if (!TextValidation(teamData.name)) toast.error('Nome do time é inválido');
+                if (!ImageValidation(teamData.picture)) toast.error('A extensão de arquivo inserida é inválida');
             }
         }
     }
@@ -130,11 +140,11 @@ export default function FormStepThree({onSubmit}) {
                     <S.InputLine>
                         <Input.Default
                             placeholder={'1A2B3C'}
-                            value={teamCode}
+                            value={teamData.code}
                             onChange={handleTeamCodeChange}
                             onFocus={handleTeamCodeTtpChange}
                             onBlur={handleTeamCodeTtpChange}
-                            disabled={teamName || category || teamPicture ? true : false}
+                            disabled={teamData.name || teamData.category || teamData.picture || teamData.local ? true : false}
                             maxLength={6}
                         />
                         {
@@ -154,11 +164,11 @@ export default function FormStepThree({onSubmit}) {
                     <S.InputLine>
                         <Input.Default
                             placeholder={'Nome do Time SC'}
-                            value={teamName}
+                            value={teamData.name}
                             onChange={handleTeamNameChange}
                             onFocus={handleTeamNameTtpChange}
                             onBlur={handleTeamNameTtpChange}
-                            disabled={teamCode ? true : false}
+                            disabled={teamData.code ? true : false}
                         />
                         {
                             !isBelow799 &&
@@ -174,9 +184,9 @@ export default function FormStepThree({onSubmit}) {
                         Categoria
                         <Input.Default
                             placeholder={'Sub-20'}
-                            value={category}
+                            value={teamData.category}
                             onChange={handleCategoryChange}
-                            disabled={teamCode ? true : false}
+                            disabled={teamData.code ? true : false}
                             width='40%'
                         />
                     </Label>
@@ -184,9 +194,10 @@ export default function FormStepThree({onSubmit}) {
                         Endereço do time
                         <S.InputLine>
                             <Input.Default
-                                value={local}
+                                value={teamData.local}
                                 placeholder={'Rua XV'}
                                 onChange={handleLocalChange}
+                                disabled={teamData.code ? true : false}
                             />
                         </S.InputLine>
                     </Label>
@@ -200,7 +211,7 @@ export default function FormStepThree({onSubmit}) {
                             onChange={handleTeamPictureChange}
                             onFocus={handleTeamPictureTtpChange}
                             onBlur={handleTeamPictureTtpChange}
-                            disabled={teamCode ? true : false}
+                            disabled={teamData.code ? true : false}
                         />
                         {
                             !isBelow799 &&
@@ -210,12 +221,12 @@ export default function FormStepThree({onSubmit}) {
                         }
                     </S.InputLine>
                 </Label>
-                <Checkbox 
+                {/* <Checkbox 
                     id={'isAmateur'} 
                     label={'Sou um time amador.'} 
                     checked={chkAmateur}
                     onClick={handleChkAmateur}
-                />
+                /> */}
             </LS.InputsContainer>
             <Button.Primary 
                 value={'Continuar'}
