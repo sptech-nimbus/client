fda55a4031574620b38100b9396ee25501ed3775
KauanMO <kauaanmatheus@gmail.com>
2024-05-14 15:24:27 -0300

    Lookup de busca de times por nome para desafio

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -16,19 +16,30 @@ import { PrimaryButton as Button } from "@components/Button/Button";
 import Utils from '@utils/Helpers';
 import { TimeValidation, TextValidation, FutureDateValidation } from '@utils/Validations';
 
+import game from '../../api/game';
+import team from '../../api/team';
+
 export default function Eventss() {
+   sessionStorage.setItem('jwt', 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYXVhYW5tYXRoZXVzQGdtYWlsLmNvbSIsImlhdCI6MTcxNTY5ODg3OX0.pH2mqkYUr5yPbrReOOSgVxVBd7KEMnTP0Dp1faNO-CWIvj6He7af7W6DP_YsDdS1b7uPmduCTSFhndRm-QgT2Q');
+   sessionStorage.setItem('teamId', 'eaeb6176-5354-41db-a303-388780fbd9c0');
+
    const dateRef = useRef();
+   const teamList = useRef();
    const [dates, setDates] = useState();
    const [datesInput, setDatesInput] = useState();
    const [eventData, setEventData] = useState({
-      name: '',
+      challenged: {
+         name: '',
+         id: ''
+      },
       type: '',
       date: '',
       time: '',
       local: '',
       description: ''
-
    });
+
+   const [teamsToChallenge, setTeamsToChallenge] = useState([]);
 
    const handleInputChange = (e) => {
       setEventData({
@@ -40,7 +51,7 @@ export default function Eventss() {
    const handleDateChange = (selectedDates) => {
       const formattedDates = selectedDates.map(date => date.format("DD/MM/YYYY"));
       setDates(formattedDates);
-      setDatesInput(selectedDates.toString().replaceAll(',', ', '))
+      setDatesInput(selectedDates.toString().replaceAll(',', ', '));
    }
 
    const SizeValidation = (text) => {
@@ -48,12 +59,11 @@ export default function Eventss() {
    }
 
    const DatesValidation = (dates) => {
-      console.log(dates);
       dates.forEach(date => {
          const splitedDate = date.split('/');
-         const formattedDate = `${splitedDate[2]}-${splitedDate[1]}-${splitedDate[0]}` 
+         const formattedDate = `${splitedDate[2]}-${splitedDate[1]}-${splitedDate[0]}`
 
-         if(!FutureDateValidation(formattedDate)) {
+         if (!FutureDateValidation(formattedDate)) {
             return false;
          }
       });
@@ -62,7 +72,7 @@ export default function Eventss() {
    }
 
    const handleDescSize = (e) => {
-      if(eventData.description.length < 300 || e.nativeEvent.inputType === 'deleteContentBackward') {
+      if (eventData.description.length < 300 || e.nativeEvent.inputType === 'deleteContentBackward') {
          setEventData({
             ...eventData,
             description: e.target.value.substring(0, 300)
@@ -71,7 +81,7 @@ export default function Eventss() {
    }
 
    const EventValidation = () => {
-      if(
+      if (
          TextValidation(eventData.description, SizeValidation) && TextValidation(eventData.local) &&
          TextValidation(eventData.name) && TextValidation(eventData.type) &&
          TimeValidation(eventData.time) && DatesValidation(dates)
@@ -83,21 +93,53 @@ export default function Eventss() {
       }
    }
 
+   const handleSearchTeams = async e => {
+      try {
+         let res = await team.byName(e.target.value, sessionStorage.getItem('jwt'));
+
+         setEventData({ ...eventData, challenged: { name: e.target.getAttribute('teamName') } });
+         setTeamsToChallenge(res.data.data || []);
+      } catch (e) {
+         toast.error(`Erro ao buscar times: ${e}`);
+      }
+   }
+
+   const handleChallenged = async e => {
+      setEventData({ ...eventData, challenged: { id: e.target.getAttribute('teamId'), name: e.target.getAttribute('teamName') } });
+      setTeamsToChallenge([]);
+      await handleCloseSeachTeams();
+   }
+
+   const handleCloseSeachTeams = async () => {
+      teamList.current.style.display = 'none';
+   }
+
+   const handleOpenSeachTeams = async e => {
+      await handleSearchTeams(e);
+
+      teamList.current.style.display = 'flex';
+   }
+
    const handleSubmit = (e) => {
       e.preventDefault();
 
-      if(EventValidation()) {
+      if (EventValidation()) {
          const events = dates.map(dateArr => {
             return {
-               ...eventData, 
+               ...eventData,
                date: dateArr
             }
          });
-         console.log(events);
-         //lógica de cadastro de evento 
+
+         events.forEach(event => {
+            if (event.type === "game") {
+               // game.post({ body: event });
+            }
+         })
+
          toast.success('Evento cadastrado!');
          setEventData({
-            name: '',
+            teamId: '',
             type: '',
             date: '',
             time: '',
@@ -107,13 +149,13 @@ export default function Eventss() {
          setDates('');
       }
       else {
-         if(!SizeValidation(eventData.description)) toast.error('A descrição do evento deve possuir no máximo 300 caracteres.');  
-         if(!TextValidation(eventData.local)) toast.error('O local do evento deve possuir pelo menos 2 caracteres.');  
-         if(!TextValidation(eventData.name)) toast.error('O nome do evento deve possuir pelo menos 2 caracteres.');  
-         if(!TextValidation(eventData.type)) toast.error('O tipo do evento deve possuir pelo menos 2 caracteres.');  
-         if(!TimeValidation(eventData.time)) toast.error('O horário inserido não é válido.');  
-         if(!DatesValidation(dates)) toast.error('As datas inseridas não são válidas.');
-      } 
+         if (!SizeValidation(eventData.description)) toast.error('A descrição do evento deve possuir no máximo 300 caracteres.');
+         if (!TextValidation(eventData.local)) toast.error('O local do evento deve possuir pelo menos 2 caracteres.');
+         if (!TextValidation(eventData.teamId)) toast.error('O nome do evento deve possuir pelo menos 2 caracteres.');
+         if (!TextValidation(eventData.type)) toast.error('O tipo do evento deve possuir pelo menos 2 caracteres.');
+         if (!TimeValidation(eventData.time)) toast.error('O horário inserido não é válido.');
+         if (!DatesValidation(dates)) toast.error('As datas inseridas não são válidas.');
+      }
    }
 
    return (
@@ -129,9 +171,9 @@ export default function Eventss() {
             theme="dark"
          />
 
-         <Sidebar page='agenda'/>
+         <Sidebar page='agenda' />
          <S.ContentContainer>
-            <Title text='Eventos' uppercase/>
+            <Title text='Eventos' uppercase />
             <S.AgendaGrid>
                <S.Container>
                   <Calendar
@@ -152,19 +194,40 @@ export default function Eventss() {
                <S.Container>
                   <S.Form onSubmit={handleSubmit}>
                      <Label>
-                        Título do evento
-                        <Input.Default 
-                           name='name'
-                           value={eventData.name}
-                           onChange={handleInputChange} 
+                        Desafiar time
+                        <Input.Default
+                           name='challenged'
+                           onInput={handleSearchTeams}
+                           value={eventData.challenged.name}
+                           onFocus={handleOpenSeachTeams}
+                           onBlur={handleCloseSeachTeams}
                         />
+                        <div ref={teamList} style={{ display: 'none', backgroundColor: '#323232', height: '6rem', overflowY: 'auto', position: 'absolute', width: '100%', bottom: '-6rem', zIndex: '1' }}>
+                           {
+                              teamsToChallenge[0]
+                                 ? teamsToChallenge.map(team => {
+                                    return team.id !== sessionStorage.getItem('teamId')
+                                       // eslint-disable-next-line react/no-unknown-property
+                                       ? <div onMouseDown={handleChallenged} teamName={team.name} teamId={team.id} key={team} style={{ height: '2rem', cursor: 'pointer', display: 'flex', flexDirection: 'column', width: '100%' }}>
+                                          <span style={{ pointerEvents: "none" }}>
+                                             {team.name}
+                                          </span>
+                                          <span style={{ pointerEvents: "none" }}>
+                                             {team.category}
+                                          </span>
+                                       </div>
+                                       : ''
+                                 })
+                                 : "Time não encontrado"
+                           }
+                        </div>
                      </Label>
                      <Label>
                         Tipo de evento
-                        <Input.Default 
+                        <Input.Default
                            name='type'
                            value={eventData.type}
-                           onChange={handleInputChange} 
+                           onChange={handleInputChange}
                         />
                      </Label>
                      <S.Flex>
@@ -189,18 +252,18 @@ export default function Eventss() {
                      </S.Flex>
                      <Label>
                         Local
-                        <Input.Default 
+                        <Input.Default
                            name='local'
                            value={eventData.local}
-                           onChange={handleInputChange} 
+                           onChange={handleInputChange}
                         />
                      </Label>
                      <Label>
                         Descricação
-                        <Input.Textarea 
+                        <Input.Textarea
                            name='description'
                            value={eventData.description}
-                           onChange={handleDescSize} 
+                           onChange={handleDescSize}
                            rows={5}
                         />
                         <S.DescSize>{eventData.description.length}/300</S.DescSize>
