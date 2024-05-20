85775707fe370f84607809e0fc0a6a51f4719c5f
YuriOlivs <yuri.silva@sptech.school>
2024-05-20 20:38:10 -0300

    feat: ajustando cadastro atraves de blobStorage

--- src/api/team.js
+++ src/api/team.js
@@ -29,16 +29,17 @@ async function getTeamsByName(name, token) {
     return response;
 }
 
-async function registerTeam(body = {}) {
-    const response = await axios.post(`${config.baseURL}/${path}`, body)
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
+async function registerTeam(body = {}, token) {
+    try {
+        const response = await axios.post(`${config.baseURL}/${path}`, { 
+            body , 
+            headers: { Authorization: `Bearer ${token}` }
         });
-
-    return response;
+        return response;
+    }
+    catch(err) {
+        throw err;
+    }
 }
 
 async function getActiveInjuries({ param }) {
--- src/pages/Register/TeamRegister.jsx
+++ src/pages/Register/TeamRegister.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import * as LS from '@pages/Login/Login.styles';
 
+import { useNotification } from "@contexts/notification";
+import { useAuth } from "@contexts/auth";
+
 import Background from '@components/Background/Background';
 
 import FormStepThree from './TrainerRegister/FormStepThree';
@@ -9,20 +12,22 @@ import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 
 import team from "@api/team";
-import blobStorage from "@api/blolStorage";
+import blobStorage from "@api/blobStorage";
 
 export default function TeamRegister() {
+    const { token, personaId } = useAuth();
+    const { addNotification } = useNotification();
     const navigate = useNavigate();
+
+    useEffect(() => { console.log(token, personaId); }, []);
     
     const [isRegisterFinished, setIsRegisterFinished] = useState(false);
-
     const [teamData, setTeamData] = useState({
         code: '',
         name: '',
         category: '',
         picture: '',
         local: '',
-
     });
 
     useEffect(() => {
@@ -34,16 +39,35 @@ export default function TeamRegister() {
         }
      }, [isRegisterFinished]);
 
-    function handleFormSubmit(formData) {
+    const handleFormSubmit = async (formData) => {
       setTeamData({
             code: formData.code,
             name: formData.name,
             category: formData.category,
             picture: formData.picture,
+            coach: {
+              id: id
+            }
       });
+      const { picture } = teamData;
+      delete teamData.picture;
 
-      team.post(teamData);
-      setIsRegisterFinished(!isRegisterFinished);
+      try {
+          await team.post(teamData, token);
+          try {
+            await blobStorage.post(picture); }
+          catch(err) {
+            addNotification('error', 'Houve um erro ao cadastrar a imagem do time. Tente novamente mais tarde.');
+          }
+          finally {
+             addNotification('success', 'Cadastro realizado! Redirecionando para tela de seleção de time...');
+            // setIsRegisterFinished(true);
+          }
+      }
+      catch(err) {
+         addNotification('error', 'Não foi possível realizar o cadastro do time, por favor tente novamente mais tarde.');
+         console.log(err);
+      }
     }
 
     return (
