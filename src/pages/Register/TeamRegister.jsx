4585440e0564483b93bd38a0a254db3e9617943d
= <kauaanmatheus@gmail.com>
2024-05-25 16:43:39 -0300

    feat: Exibição de próxima partida

--- src/pages/Register/TeamRegister.jsx
+++ src/pages/Register/TeamRegister.jsx
@@ -15,70 +15,69 @@ import team from "@api/team";
 import blobStorage from "@api/blobStorage";
 
 export default function TeamRegister() {
-    const { token, personaId, id } = useAuth();
-    const { addNotification } = useNotification();
-    const navigate = useNavigate();
-    
-    const [isRegisterFinished, setIsRegisterFinished] = useState(false);
-    const [teamData, setTeamData] = useState({coach: { id: localStorage.getItem('personaId') }});
+  const { token, personaId, id } = useAuth();
+  console.log(token);
+  const { addNotification } = useNotification();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if(isRegisterFinished) {
-           toast.success('Cadastro realizado!', { autoClose: 5000 });
-           setTeamData({});
-          //  setTimeout(() => {
-          //       navigate('/my-teams');
-          //  }, 2600);
+  const [isRegisterFinished, setIsRegisterFinished] = useState(false);
+  const [teamData, setTeamData] = useState({ coach: { id: localStorage.getItem('personaId') } });
+
+  useEffect(() => {
+    if (isRegisterFinished) {
+      toast.success('Cadastro realizado!', { autoClose: 5000 });
+      setTeamData({});
+      //  setTimeout(() => {
+      //       navigate('/my-teams');
+      //  }, 2600);
+    }
+  }, [isRegisterFinished]);
+
+  const handleFormSubmit = async (formData) => {
+    teamData.name = formData.name;
+    teamData.category = formData.category;
+    teamData.local = formData.local;
+    const { picture } = formData;
+    delete teamData.picture;
+
+    try {
+      const { data } = await team.post(teamData, token);
+      if (picture) {
+        try {
+          await blobStorage.post(picture, localStorage.getItem('token'), data.data.id);
         }
-     }, [isRegisterFinished]);
-
-    const handleFormSubmit = async (formData) => {
-      teamData.name = formData.name;
-      teamData.category = formData.category;
-      teamData.local = formData.local;
-      const { picture }  = formData;
-      delete teamData.picture;
-
-      console.log(teamData);
-      console.log(picture);
-      try {
-          const { data } = await team.post(teamData, token);
-          if(picture) {
-            try {
-              await blobStorage.post(picture, localStorage.getItem('token'), data.data.id);
-            }
-            catch(err) {
-              console.log(err);
-              addNotification("error", "Seu time foi cadastrado, porém houve um erro ao cadastrarmos a imagem dele. Por favor tente novamente em outro momento.")
-            }
-            finally {
-              setIsRegisterFinished(true);
-            }
-          }
-      }
-      catch(err) {
-         console.log(err);
-         addNotification("error", "Houve um erro ao cadastrar o time. Por favor, aguarde um momento antes de tentar novamente");
+        catch (err) {
+          console.log(err);
+          addNotification("error", "Seu time foi cadastrado, porém houve um erro ao cadastrarmos a imagem dele. Por favor tente novamente em outro momento.")
+        }
+        finally {
+          setIsRegisterFinished(true);
+        }
       }
     }
+    catch (err) {
+      console.log(err);
+      addNotification("error", "Houve um erro ao cadastrar o time. Por favor, aguarde um momento antes de tentar novamente");
+    }
+  }
 
-    return (
-        <LS.Header>
-            <ToastContainer
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                theme="dark"
-                limit={3}
-            />
+  return (
+    <LS.Header>
+      <ToastContainer
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="dark"
+        limit={3}
+      />
 
-            <Background.Login />
-            <LS.Title>Cadastro</LS.Title>
-            <FormStepThree onSubmit={handleFormSubmit} />
-        </LS.Header>
-    )
+      <Background.Login />
+      <LS.Title>Cadastro</LS.Title>
+      <FormStepThree onSubmit={handleFormSubmit} />
+    </LS.Header>
+  )
 }
