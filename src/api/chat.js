98991fcd23ddeb73d082062e7857d4fb1179daaa
zzzmikej <michaelhenrique0022@gmail.com>
2024-10-31 17:14:43 -0300

    feat: ajustando configuração do chat

--- src/api/chat.js
+++ src/api/chat.js
@@ -5,7 +5,7 @@ import config from "./config";
 
 export async function getMessages(teamId, page, elements) {
     try {
-        const res = await axios.get(`http://nimbus.westus.cloudapp.azure.com:3001/messages/${teamId}?page=${page}&elements=${elements}`);
+        const res = await axios.get(`http://${config.baseURL}:3001/messages/${teamId}?page=${page}&elements=${elements}`);
         return res;
     } catch (e) {
         console.log(e);
