0d646bfbf015d2483014fc578074af5c5558a868
zzzmikej <michaelhenrique0022@gmail.com>
2024-11-06 10:59:32 -0300

    fix: http for pages

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import config from "../api/config";
 
-const URL = `http://${config.baseURLChat}:3001/`;
+const URL = `${config.baseURLChat}:3001/`;
 
 export const socket = io(URL, {
     autoConnect: false
